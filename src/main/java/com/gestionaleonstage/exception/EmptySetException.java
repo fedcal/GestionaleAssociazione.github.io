7131144e654c4d42794e615a94b51dc34d9a161e
fedcal <fedcal01@gmail.com>
2022-12-10 15:37:18 +0100

    [7 - Create JavaDoc] - add exception documentation

--- src/main/java/com/gestionaleonstage/exception/EmptySetException.java
+++ src/main/java/com/gestionaleonstage/exception/EmptySetException.java
@@ -1,7 +1,18 @@
 package com.gestionaleonstage.exception;
 
+/**
+ * Classe per la gestione dell'eccezione relativa a un set di dati vuoto
+ */
 public class EmptySetException extends Exception{
+    /**
+     * Costruttore di classe vuoto
+     */
     public EmptySetException(){}
+
+    /**
+     * Costruttore di classe con un messaggio in input per visualizzarlo in caso di errore
+     * @param msg String
+     */
     public EmptySetException(String msg) {
         super(msg);
     }
